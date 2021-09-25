0b3adf882cd38952b7f9ef1ba52cd2a8507c620e
shivaayy <45315896+shivaayy@users.noreply.github.com>
2021-09-25 17:34:42 +0530

    done changes

--- temperature_meter.ino
+++ temperature_meter.ino
@@ -3,47 +3,42 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+int temperatue_sensor_1_pin = 2;
+int temperatue_sensor_2_pin = 3;
+int temperatue_sensor_3_pin = 4;
+//int temperatue_sensor_4_pin = 5;
 
-int temperatue_sensor_1_pin=2;
-int temperatue_sensor_2_pin=3;
-int temperatue_sensor_3_pin=4;
-int temperatue_sensor_4_pin=5;
+float temperature_1_value = 0;
+float temperature_2_value = 0;
+float temperature_3_value = 0;
+//float temperature_4_value = 0;
 
-float temperature_1_value=0;
-float temperature_2_value=0;
-float temperature_3_value=0;
-float temperature_4_value=0;
-
-long int start_millisecond;
-long int current_millisecond;
+unsigned long int start_millisecond;
+unsigned long int current_millisecond;
 
 int hours;
 int minutes;
 int seconds;
 
-byte degreeC[]={
-  B11100,
-  B10100,
-  B11100,
-  B00111,
-  B01000,
-  B01000,
-  B01000,
-  B00111
-  };
+byte degreeC[] = {
+    B11100,
+    B10100,
+    B11100,
+    B00111,
+    B01000,
+    B01000,
+    B01000,
+    B00111};
 
-  byte water[] = {
-  B00100,
-  B01010,
-  B01010,
-  B10001,
-  B10001,
-  B10001,
-  B01110,
-  B00000
-};
-
-
+byte water[] = {
+    B00100,
+    B01010,
+    B01010,
+    B10001,
+    B10001,
+    B10001,
+    B01110,
+    B00000};
 
 //byte coil[] = {
 //  B00011,
@@ -56,165 +51,172 @@ byte degreeC[]={
 //  B01110
 //};
 byte tank1[] = {
-  B00001,
-  B01001,
-  B11001,
-  B01001,
-  B01001,
-  B11101,
-  B00001,
-  B11111 
-};
+    B00001,
+    B01001,
+    B11001,
+    B01001,
+    B01001,
+    B11101,
+    B00001,
+    B11111};
 byte tank2[] = {
-  B00001,
-  B11101,
-  B00101,
-  B11101,
-  B10001,
-  B11101,
-  B00001,
-  B11111
-};
+    B00001,
+    B11101,
+    B00101,
+    B11101,
+    B10001,
+    B11101,
+    B00001,
+    B11111};
 
 OneWire oneWirePin_1(temperatue_sensor_1_pin);
 OneWire oneWirePin_2(temperatue_sensor_2_pin);
 OneWire oneWirePin_3(temperatue_sensor_3_pin);
 //OneWire oneWirePin_4(temperatue_sensor_4_pin);
 
+DallasTemperature sensor_1(&oneWirePin_1);
+DallasTemperature sensor_2(&oneWirePin_2);
+DallasTemperature sensor_3(&oneWirePin_3);
+//DallasTemperature sensor_4(&oneWirePin_4);
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-DallasTemperature sensor_1(&oneWirePin_1); 
-DallasTemperature sensor_2(&oneWirePin_2); 
-DallasTemperature sensor_3(&oneWirePin_3); 
-//DallasTemperature sensor_4(&oneWirePin_4); 
-
-
-
-LiquidCrystal_I2C lcd(0x27,16,2);
-
-void setup() {
+void setup()
+{
 
   Serial.begin(9600);
 
-  start_millisecond=millis();
+  start_millisecond = millis();
+  Serial.print("start millis-------------  ");
+  Serial.print(start_millisecond);
 
   sensor_1.begin();
   sensor_2.begin();
   sensor_3.begin();
-//  sensor_4.begin();
-  
-  lcd.begin();
-   lcd.createChar(0,degreeC);
-  lcd.createChar(1,tank1);
-  lcd.createChar(2,tank2);
-  lcd.createChar(3,water);
-//  lcd.createChar(4,coil);
-  
-  lcd.home();
-//  lcd.backlight(); 
+  //  sensor_4.begin();
 
+  lcd.begin();
+  lcd.createChar(0, degreeC);
+  lcd.createChar(1, tank1);
+  lcd.createChar(2, tank2);
+  lcd.createChar(3, water);
+  //  lcd.createChar(4,coil);
+
+  lcd.home();
+  //  lcd.backlight();
 }
 
-void loop() {
+void loop()
+{
 
   Serial.print("\n Requesting Temperatures from sensors-----------  ");
 
   sensor_1.requestTemperatures();
   sensor_2.requestTemperatures();
   sensor_3.requestTemperatures();
-//  sensor_4.requestTemperatures();
-  
-  Serial.print("Done  \n "); 
+  //  sensor_4.requestTemperatures();
 
-  temperature_1_value=sensor_1.getTempCByIndex(0);
-  temperature_2_value=sensor_2.getTempCByIndex(0);
-  temperature_3_value=sensor_3.getTempCByIndex(0);
-//  temperature_4_value=sensor_4.getTempCByIndex(0);
+  Serial.print("Done  \n ");
 
+  temperature_1_value = sensor_1.getTempCByIndex(0);
+  temperature_2_value = sensor_2.getTempCByIndex(0);
+  temperature_3_value = sensor_3.getTempCByIndex(0);
+  //  temperature_4_value=sensor_4.getTempCByIndex(0);
 
   lcd.clear();
-  
+
   // printing degree C symbol
-  lcd.setCursor(6,0);
+  lcd.setCursor(6, 0);
   lcd.write(0);
-  lcd.setCursor(15,0);
+  lcd.setCursor(15, 0);
   lcd.write(0);
-  lcd.setCursor(6,1);
+  lcd.setCursor(6, 1);
   lcd.write(0);
-//  lcd.setCursor(15,1);
-//  lcd.write(0);
+  //  lcd.setCursor(15,1);
+  //  lcd.write(0);
 
   // printing tank1 symbol
 
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.write(1);
 
   // printing tank2 symbol
 
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.write(2);
 
   // printing water symbol
 
-  lcd.setCursor(8,0);
+  lcd.setCursor(8, 0);
   lcd.write(3);
 
   // printing coil symbol
 
   // lcd.setCursor(8,1);
   // lcd.write(4);
-  
 
-
-  
-
-  lcd.setCursor(1,0);
+  lcd.setCursor(1, 0);
   lcd.print(temperature_1_value);
-  lcd.setCursor(9,0);
+  lcd.setCursor(9, 0);
   lcd.print(temperature_2_value);
-  lcd.setCursor(1,1);
+  lcd.setCursor(1, 1);
   lcd.print(temperature_3_value);
   // lcd.setCursor(9,1);
   // lcd.print(temperature_4_value);
-
-  int millis_diff=current_millisecond-start_millisecond;
-  hours=millis_diff/3600000;
-  minutes=(millis_diff%3600000)/60000;
-  seconds=((millis_diff%3600000)%60000)/1000;
-  lcd.setCursor(13,1);
+  current_millisecond = millis();
+  unsigned long int millis_diff = current_millisecond - start_millisecond;
+  Serial.print("current millis-------------  ");
+  Serial.print(current_millisecond);
+  hours = millis_diff / 3600000UL;
+  minutes = (millis_diff % 3600000UL) / 60000UL;
+  seconds = ((millis_diff % 3600000UL) % 60000UL) / 1000UL;
+  lcd.setCursor(13, 1);
   lcd.print(":");
-  lcd.setCursor(10,1);
+  lcd.setCursor(10, 1);
   lcd.print(":");
-  lcd.setCursor(14,1);
+  if (seconds >= 10)
+    lcd.setCursor(14, 1);
+  else
+  {
+    lcd.setCursor(14, 1);
+    lcd.print(0);
+    lcd.setCursor(15, 1);
+  }
   lcd.print(seconds);
-   lcd.setCursor(11,1);
+  if (minutes >= 10)
+    lcd.setCursor(11, 1);
+  else
+  {
+    lcd.setCursor(11, 1);
+    lcd.print(0);
+    lcd.setCursor(12, 1);
+  }
   lcd.print(minutes);
-  if(hours>=10)
-   lcd.setCursor(8,1);
-   else
-  lcd.setCursor(9,1);
+  if (hours >= 10)
+    lcd.setCursor(8, 1);
+  else
+  {
+    lcd.setCursor(8, 1);
+    lcd.print(0);
+    lcd.setCursor(9, 1);
+  }
   lcd.print(hours);
 
- delay(800);
-  
+  delay(1000);
 
-//  Serial.print("Temperature is ----   ");
-//  Serial.print(temperature);
-  
-//  lcd.setCursor(0,0);
-//  lcd.print(temperature_1_value);
-//  
-//  lcd.setCursor(8,0);
-//  lcd.print(temperature_2_value);
-//  
-//  lcd.setCursor(0,1);
-//  lcd.print(temperature3_value);
-//
-//  
-//  lcd.setCursor(8,1);
-//  lcd.print(temperature_4_value);
-  
-  
-delay(200);
+  //  Serial.print("Temperature is ----   ");
+  //  Serial.print(millis_diff);
 
+  //  lcd.setCursor(0,0);
+  //  lcd.print(temperature_1_value);
+  //
+  //  lcd.setCursor(8,0);
+  //  lcd.print(temperature_2_value);
+  //
+  //  lcd.setCursor(0,1);
+  //  lcd.print(temperature3_value);
+  //
+  //
+  //  lcd.setCursor(8,1);
+  //  lcd.print(temperature_4_value);
 }
